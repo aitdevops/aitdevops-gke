a3dcc66abd5b5f51554c2c04da2c5f5a4fd11bdd
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-07-26 12:35:03 -0400

    svc

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const productServiceUrl = 'http://aitdevops-site-product-service.default.svc.cluster.local';
-    const orderServiceUrl = 'http://aitdevops-site-order-service.default.svc.cluster.local';
-    const userServiceUrl = 'http://aitdevops-site-user-service.default.svc.cluster.local';
+    const productServiceUrl = 'http://10.0.19.213:5001';
+    const orderServiceUrl = 'http://10.0.20.243:5002';
+    const userServiceUrl = 'http://10.0.22.225:5003';
     
 
     document.getElementById('fetch-products').addEventListener('click', function() {
